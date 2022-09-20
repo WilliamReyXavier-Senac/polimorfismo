b8cd13046b9b131cfc6b60d91b0eb2ab126831a8
William Xavier <willian.xavier@alunos.sc.senac.br>
2022-09-19 21:03:52 -0300

    Animais

--- src/polimorfismo/Calculadora.java
+++ src/polimorfismo/Calculadora.java
@@ -13,20 +13,17 @@ public class Calculadora {
     
     public int somar(int v1, int v2) {
         int soma = v1 + v2;
-        System.out.println(soma);
         return soma;
     }
     
     public int somar(int v1, int v2, int v3) {
         int soma = v1 + v2 + v3;
-        System.out.println(soma);
         return soma;
     }
     
     
     public double somar(double v1, double v2) {
         double soma = v1 + v2;
-        System.out.println(soma);
         return soma;
     }
     
--- src/polimorfismo/Polimorfismo.java
+++ src/polimorfismo/Polimorfismo.java
@@ -3,6 +3,16 @@ package polimorfismo;
 public class Polimorfismo {
     
     public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        //System.out.println(calc.somar(2, 2.3));
+        
+        Gato gato = new Gato();
+        Cachorro cachorro = new Cachorro();
+        Pato pato = new Pato();
+        
+        gato.comunicar();
+        cachorro.comunicar();
+        pato.comunicar();
         
     }
     
